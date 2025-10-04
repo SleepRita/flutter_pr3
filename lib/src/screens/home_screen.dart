3d8cd31ab8d6e372daa9b74ced77584f8dcb574d
Bagautdinova Margarita <margarita.bagautdinova_2004@mail.ru>
2025-10-04 09:29:50 +0300

    Second commit

---
+++ lib/src/screens/home_screen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int happiness = 80;
+  int hunger = 60;
+  final String petName = "Барсик";
+
+  void playWithPet() {
+    setState(() {
+      happiness = (happiness + 10).clamp(0, 100);
+    });
+  }
+
+  void feedPet() {
+    setState(() {
+      hunger = (hunger - 15).clamp(0, 100);
+      happiness = (happiness + 5).clamp(0, 100);
+    });
+  }
+
+  void washPet() {
+    setState(() {
+      hunger = (hunger + 5).clamp(0, 100);
+      happiness = (happiness - 20).clamp(0, 100);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Мой питомец: $petName'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('Текущее состояние:', style: TextStyle(fontSize: 22)),
+            const SizedBox(height: 20),
+            Text('Уровень счастья: $happiness / 100', style: const TextStyle(fontSize: 18)),
+            Text('Уровень сытости: ${100 - hunger} / 100', style: const TextStyle(fontSize: 18)),
+            const SizedBox(height: 30),
+            // Основные действия
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(onPressed: feedPet, child: const Text('Покормить')),
+                const SizedBox(width: 20),
+                ElevatedButton(onPressed: playWithPet, child: const Text('Поиграть')),
+                const SizedBox(width: 20),
+                ElevatedButton(onPressed: washPet, child: const Text('Помыть')),
+              ],
+            ),
+            const SizedBox(height: 40),
+            const Divider(), // Разделитель
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text('Меню:', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            ),
+            // Навигация
+            Wrap(
+              spacing: 10,
+              runSpacing: 10,
+              children: [
+
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_pr3/main.dart';
+import 'package:flutter_pr3/src/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
